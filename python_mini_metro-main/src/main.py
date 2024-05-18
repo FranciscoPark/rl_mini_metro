2e8b532d6be36158fbf2cfe80ebe8c79e943519c
FranciscoPark <76905169+FranciscoPark@users.noreply.github.com>
2024-05-18 22:52:28 +0900

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch francisco
    Your branch is up to date with 'origin/francisco'.
    
    Changes to be committed:
    	modified:   python_mini_metro-main/src/__pycache__/mediator.cpython-39.pyc
    	modified:   python_mini_metro-main/src/main.py

--- python_mini_metro-main/src/main.py
+++ python_mini_metro-main/src/main.py
@@ -22,9 +22,7 @@ game_states = []
 # Configurations
 running = True
 save_states = True
-human_player = False
 enable_graphics = True
-# assert screen is shown for human players
 
 
 while running:
@@ -39,67 +37,18 @@ while running:
         mediator.render(screen)
     running = mediator.is_gameover()
 
-
-     # save game state in dict
-    if mediator.steps % 1000 == 0 and save_states:
-        current_state = mediator.save_state()
-        if len(game_states) > 0:
-
-            # compute reward and action
-            previous_score = game_states[-1]['score']
-            current_score = current_state['score']
-            game_states[-1]['reward'] = current_score - previous_score
-
-            # Detect new links in paths by comparing adjacency matrices
-            new_links = {}
-            if len(game_states) > 0:
-                for i,color in enumerate(['red','green','blue']):
-                    new_links_p = []
-                    adj_matrix_prev = game_states[-1]['paths_adj_matrix'][color]
-                    adj_matrix_curr = current_state['paths_adj_matrix'][color]
-                    
-                    for j in range(len(adj_matrix_prev)):
-                        for k in range(len(adj_matrix_prev)):
-                            if adj_matrix_prev[j][k] != adj_matrix_curr[j][k] and j < k:
-                                new_links_p.append((game_states[-1]['station_ids'][j],game_states[-1]['station_ids'][k]))
-                    new_links[color] = new_links_p
-            game_states[-1]['action'] = new_links # This is just a placeholder/proxy for the actual action space we will define later
-            
-        game_states.append(current_state)
-        print(current_state['score'])
-        print(current_state['station_passengers'])
    
-   
-    # react to user interaction
-    if human_player:
-        for pygame_event in pygame.event.get():
-            if pygame_event.type == pygame.QUIT:
-
-                raise SystemExit
-            else:
-                event = convert_pygame_event(pygame_event)
-                mediator.react(event)
-
-        pygame.display.flip()
-    else:
-        # add station
-        if mediator.steps >= 100 and mediator.steps < 200:
-            mediator.agent_add_station_to_path(mediator.paths[0],mediator.stations[0])
-
-
-        if enable_graphics:
-            for pygame_event in pygame.event.get():
-                if pygame_event.type == pygame.QUIT:
-                    raise SystemExit
-            pygame.display.flip()
+    for pygame_event in pygame.event.get():
+        if pygame_event.type == pygame.QUIT:
+            raise SystemExit
+        else:
+            event = convert_pygame_event(pygame_event)
+            mediator.react(event)
+    pygame.display.flip()
+    
             
 
-print("-------- Got here")
+
 pygame.time.delay(1000) # 2초 딜레이 (ms기준)
 pygame.quit()
 
-# Save game states to a JSON file
-current_datetime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
-filename = f"{current_datetime}.json"
-with open(filename, 'w') as file:
-    json.dump(game_states, file)
